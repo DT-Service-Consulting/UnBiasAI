f376fbf48c0902d3fbfe7eb8c015fbb42770da9a
Marco <marco@mdigennaro.net>
2025-05-02 11:37:35 +0200

    modif config

--- src/unbiasai/config.py
+++ src/unbiasai/config.py
@@ -4,10 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent
 
 # Paths to various resources
-IMAGES_DIR = BASE_DIR / 'images'
 DATA_DIR = BASE_DIR / 'data'
-RESULTS_DIR = DATA_DIR / 'results'
-SAVED_MODELS_DIR = DATA_DIR / 'saved_models'
 
 # from google.colab import userdata
 # OPENAI_API_KEY = userdata.get('OpenAIKey')
